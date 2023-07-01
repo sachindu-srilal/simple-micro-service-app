266c81f378440c6a600a7f1d4c976690abc1ee4f
Sachindu Srilal <lal95.ac@gmail.com>
2023-07-01 13:58:57 +0530

    Setup the springboot

--- back-end/src/main/java/lk/ijse/dep10/backend/BackEndApplication.java
+++ back-end/src/main/java/lk/ijse/dep10/backend/BackEndApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackEndApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello spring");
 		SpringApplication.run(BackEndApplication.class, args);
 	}
 
